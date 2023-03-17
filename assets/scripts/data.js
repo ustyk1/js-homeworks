2ecad87ca74e7a3f9728301554a8cf9709b80606
ustyk1 <eugenia.ustyk@gmail.com>
2023-03-17 21:25:29 +0200

    add homework shopping-list

---
+++ assets/scripts/data.js
@@ -0,0 +1,146 @@
+export const products = [
+  {
+    id: 'kiwi',
+    nameOfProduct: 'Kiwi',
+    amount: 1,
+    isBought: false,
+    price: 1.5,
+    path: './assets/styles/svg/kiwi.svg'
+  },
+  {
+    id: 'apricot',
+    nameOfProduct: 'Apricot',
+    amount: 1,
+    isBought: false,
+    price: 1,
+    path: './assets/styles/svg/apricot.svg'
+  },
+  {
+    id: 'avocado',
+    nameOfProduct: 'Avocado',
+    amount: 1,
+    isBought: false,
+    price: 4,
+    path: './assets/styles/svg/avocado.svg'
+  },
+  {
+    id: 'carrot',
+    nameOfProduct: 'Carrot',
+    amount: 1,
+    isBought: false,
+    price: 0.75,
+    path: './assets/styles/svg/carrot.svg'
+  },
+  {
+    id: 'cherry-branch',
+    nameOfProduct: 'Cherry-branch',
+    amount: 1,
+    isBought: false,
+    price: 0.15,
+    path: './assets/styles/svg/cherry-branch.svg'
+  },
+  {
+    id: 'grape-blue',
+    nameOfProduct: 'Grape-blue',
+    amount: 1,
+    isBought: false,
+    price: 8,
+    path: './assets/styles/svg/grape-blue.svg'
+  },
+  {
+    id: 'olive-branch',
+    nameOfProduct: 'Olive-branch',
+    amount: 1,
+    isBought: false,
+    price: 0.1,
+    path: './assets/styles/svg/olive-branch.svg'
+  },
+  {
+    id: 'orange',
+    nameOfProduct: 'Orange',
+    amount: 1,
+    isBought: false,
+    price: 2,
+    path: './assets/styles/svg/orange.svg'
+  },
+  {
+    id: 'onion',
+    nameOfProduct: 'Onion',
+    amount: 1,
+    isBought: false,
+    price: 0.3,
+    path: './assets/styles/svg/onion.svg'
+  },
+  {
+    id: 'strawberry',
+    nameOfProduct: 'Strawberry',
+    amount: 1,
+    isBought: false,
+    price: 0.25,
+    path: './assets/styles/svg/strawberry.svg'
+  },
+  {
+    id: 'potato',
+    nameOfProduct: 'Potato',
+    amount: 1,
+    isBought: false,
+    price: 0.15,
+    path: './assets/styles/svg/potato.svg'
+  },
+  {
+    id: 'cabbage',
+    nameOfProduct: 'Cabbage',
+    amount: 1,
+    isBought: false,
+    price: 2,
+    path: './assets/styles/svg/cabbage.svg'
+  },
+  {
+    id: 'cucumber',
+    nameOfProduct: 'Cucumber',
+    amount: 1,
+    isBought: false,
+    price: 1.75,
+    path: './assets/styles/svg/cucumber.svg'
+  },
+  {
+    id: 'grape-green',
+    nameOfProduct: 'Grape-green',
+    amount: 1,
+    isBought: false,
+    price: 6,
+    path: './assets/styles/svg/grape-green.svg'
+  },
+  {
+    id: 'apple',
+    nameOfProduct: 'Apple',
+    amount: 1,
+    isBought: false,
+    price: 0.9,
+    path: './assets/styles/svg/apple.svg'
+  },
+  {
+    id: 'tomato',
+    nameOfProduct: 'Tomato',
+    amount: 1,
+    isBought: false,
+    price: 1,
+    path: './assets/styles/svg/tomato.svg'
+  },
+  {
+    id: 'donut',
+    nameOfProduct: 'Donut',
+    amount: 1,
+    isBought: false,
+    price: 0.9,
+    path: './assets/styles/svg/donut.svg'
+  },
+  {
+    id: 'hamburger',
+    nameOfProduct: 'Hamburger',
+    amount: 1,
+    isBought: false,
+    price: 0.9,
+    path: './assets/styles/svg/hamburger.svg'
+  },
+];
